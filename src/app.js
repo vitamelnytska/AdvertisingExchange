bf9e7b37d883e9f5fc6bb6d2cd4561d8b6f8cb79
vladislav2002 <57789590+vladislav2002@users.noreply.github.com>
2021-03-17 22:29:13 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const a = 1;
+
+function foo(name) {
+  const lastName = name;
+  return lastName;
+}
+
+foo('Denis');
+const x = 200;
+
+console.log(x, a);
+
+const arrow = (y) => y + 1;
+arrow(2);
+
+const obj = {
+  name: 'Denis',
+};
+
+const name = obj.name;
