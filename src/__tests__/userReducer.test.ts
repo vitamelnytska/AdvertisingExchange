1f8e0fb8c3652b992d7558e1f3a1d51596e1e569
vitamelnytska <vita.melnytska@gmail.com>
2021-05-24 22:56:33 +0300

    fix testing user reducer

--- src/__tests__/userReducer.test.ts
+++ src/__tests__/userReducer.test.ts
@@ -1,45 +1,43 @@
 import { Draft } from "immer";
-import { setUserData, UserActions } from "../store/ducks/user/actionCreators";
-import { UserActionsType } from "../store/ducks/user/contracts/actionTypes";
+import { setUserData, setUserLoadingStatus, signOut} from "../store/ducks/user/actionCreators";
 import { User, UserState } from "../store/ducks/user/contracts/state";
 import { userReducer } from "../store/ducks/user/userReducer";
 import { LoadingStatus } from "../store/types";
-import {
-    FetchSignInActionInterface,
-    FetchSignUpActionInterface,
-    FetchUserDataActionInterface,
-    SetUserDataActionInterface,
-    SetUserLoadingStatusActionInterface,
-    SignOutActionInterface,
-  } from "../store/ducks/user/contracts/actionTypes";
-test(" ", () => {
-    const user: User = {
-        _id: "02inkjnf9j",
-        email: "firstname.fullname@gmail.com",
-        fullname: "fullname",
-        username: "username",
-        password: "password",
-        confirmHash: "confirm",
-        confirmed: true,
-        location: "Kyiv",
-        about: "nothing about",
-        website: "https://www.pinterest.ru/pin/662662532652814539/",
-    };
-    const statusLoad = LoadingStatus.LOADED;
 
-    const draft: Draft<UserState> = {
-        data: user,
-        status: statusLoad,
-    };
+const user: User = {
+    _id: "02inkjnf9j",
+    email: "firstname.fullname@gmail.com",
+    fullname: "fullname",
+    username: "username",
+    password: "password",
+    confirmHash: "confirm",
+    confirmed: true,
+    location: "Kyiv",
+    about: "nothing about",
+    website: "https://www.pinterest.ru/pin/662662532652814539/",
+};
+const statusLoad = LoadingStatus.LOADED;
 
-    const usAction: UserActions = 
-    | SetUserDataActionInterface
-    | SetUserLoadingStatusActionInterface
-    | FetchUserDataActionInterface
-    | SignOutActionInterface;
+const draft: Draft<UserState> = {
+    data: user,
+    status: statusLoad,
+};
 
-    userReducer(draft, UserActionsType)
+test("SET_USER_DATA", () => {
+    const newState = userReducer(draft, setUserData(user));
+    // expect(newState.status).toBe(LoadingStatus.LOADED); // false
+    expect(newState.status).toBe(LoadingStatus.SUCCESS); // true
+    expect(newState.data).toBeTruthy();
+});
 
-    expect();
+test("SET_LOADING_STATE", () => {
+    const newState = userReducer(draft, setUserLoadingStatus(statusLoad));
+    expect(newState.status).toBeTruthy(); // LOADED is LOADED
+});
+
+test("SIGN_OUT", () => {
+    const newState = userReducer(draft, signOut());
+    expect(newState.data).toBe(undefined);
+    expect(newState.status).toBe(LoadingStatus.LOADED);
 });
 
